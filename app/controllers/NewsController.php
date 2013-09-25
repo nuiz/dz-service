cc9a256e46fad49d5bfe1896723b190cca69b5ac
nuiz <papangping@gmail.com>
2013-09-25 20:09:59 +0700

    finish news picture, video

--- app/controllers/NewsController.php
+++ app/controllers/NewsController.php
@@ -21,10 +21,14 @@ class NewsController extends BaseController {
             $news = $paging->getCollection();
             $data = $news->toArray();
             $pictures_id = $news->lists('picture_id');
+            $videos_id = $news->lists('video_id');
             $news_id = $news->lists('id');
 
             if(count($pictures_id)>0){
                 $pictures = Picture::whereIn('id', $pictures_id)->get();
+            }
+            if(count($videos_id)>0){
+                $videos = NewsVideo::whereIn('id', $videos_id)->get();
             }
             if(count($data)> 0 & $this->_isset_field('like')){
                 $likes = Like::whereIn('id', $news_id)->get();
@@ -40,6 +44,18 @@ class NewsController extends BaseController {
                         $buffer2 = $buffer->first()->toArray();
                         $buffer2['link'] = URL::to('picture/'.$buffer2['picture_link']);
                         $data[$key]['picture'] = $buffer2;
+                    }
+                }
+                if($videos_id>0){
+                    $buffer = $videos->filter(function($item) use ($value){
+                        if($value['video_id']==$item->id){
+                            return true;
+                        }
+                    });
+                    if($buffer->count()>0){
+                        $buffer2 = $buffer->first()->toArray();
+                        $buffer2['link'] = URL::to('news_video/'.$buffer2['video_link']);
+                        $data[$key]['video'] = $buffer2;
                     }
                 }
                 if($this->_isset_field('like')){
@@ -89,8 +105,16 @@ class NewsController extends BaseController {
     {
         try {
             $item = News::findOrFail($id)->toArray();
-            $item['picture'] = Picture::findOrFail($item['picture_id'])->toArray();
-            $item['picture']['link'] = URL::to('picture/'.$item['picture']['picture_link']);
+            $picture = Picture::find($item['picture_id']);
+            if(!is_null($picture)){
+                $item['picture'] = $picture->toArray();
+                $item['picture']['link'] = URL::to('picture/'.$item['picture']['picture_link']);
+            }
+            $video = NewsVideo::find($item['video_id']);
+            if(!is_null($video)){
+                $item['video'] = $video->toArray();
+                $item['video']['link'] = URL::to('news_video/'.$item['video']['video_link']);
+            }
 
             if($this->_isset_field('like')){
                 $item['like'] = Like::find($id)->toArray();
@@ -126,23 +150,50 @@ class NewsController extends BaseController {
                 $news = new News();
                 $news->name = Input::get('name');
                 $news->message = Input::get('message');
+                $news->media_type = 'none';
 
-                if(Input::hasFile('picture')){
-                    $picture = new Picture();
-                    list($width, $height, $type, $attr) = getimagesize(Input::file('picture')->getRealPath());
-                    $picture->size_x = $width;
-                    $picture->size_y = $height;
-                    $picture->save();
+                if(Input::hasFile('media')){
+                    $media = Input::file('media');
+                    $ext = strtolower($media->getClientOriginalExtension());
 
-                    $name = $picture->id.'.'.Input::file('picture')->getClientOriginalExtension();
-                    Input::file('picture')->move('picture', $name);
-                    chmod('picture/'.$name, 0777);
+                    $pic_allows = array('jpg', 'jpeg', 'png');
+                    $video_allows = array('mp4');
 
-                    $picture->picture_link = $name;
-                    $picture->save();
-                    $news->picture_id = $picture->id;
+                    if(in_array($ext, $pic_allows)){
+                        $picture = new Picture();
+                        list($width, $height, $type, $attr) = getimagesize($media->getRealPath());
+                        $picture->size_x = $width;
+                        $picture->size_y = $height;
+                        $picture->save();
+
+                        $name = $picture->id.'.'.$ext;
+                        $media->move('picture', $name);
+                        chmod('picture/'.$name, 0777);
+
+                        $picture->picture_link = $name;
+                        $picture->save();
+
+                        $news->picture_id = $picture->id;
+                        $news->media_type = 'picture';
+                    }
+                    else if(in_array($ext, $video_allows)){
+                        $news_video = new NewsVideo();
+                        $news_video->save();
+
+                        $name = $news_video->id.'.'.$ext;
+                        $media->move('news_video', $name);
+                        chmod('news_video/'.$name, 0777);
+
+                        $news_video->video_link = $name;
+                        $news_video->save();
+
+                        $news->video_id = $news_video->id;
+                        $news->media_type = 'video';
+                    }
+                    else {
+                        throw new Exception('media type not allow');
+                    }
                 }
-
                 $news->save();
             });
             return Response::json($res);
---
+++ app/models/NewsVideo.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: P2DC
+ * Date: 25/9/2556
+ * Time: 19:04 น.
+ * To change this template use File | Settings | File Templates.
+ */
+
+class NewsVideo extends DZEloquent {
+    protected $_dz_type = 'news_video';
+    protected $table = 'news_video';
+}
